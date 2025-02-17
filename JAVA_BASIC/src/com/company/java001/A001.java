3b5b7c67fd5bd0050b3fa65e86ef07e06da152a2
modoclou <cyj4336@gmail.com>
2025-02-17 17:57:28 +0900

    third commit 20250217

---
+++ JAVA_BASIC/src/com/company/java001/A001.java
@@ -0,0 +1,22 @@
+package com.company.java001;
+
+public class A001 {
+	public static void main(String [] args) {
+		System.out.println("Hello");
+	} // end main
+} // end class
+
+// 한줄 주석(설명글)
+/*  ctrl + (화면 크게) / ctrl - (화면 작게)
+
+ 3번째줄 : public class A001{
+     public : 아무데서나 접근 가능 / class : 부품 객체 / A001 : 클래스이름
+ 4번째줄 : jvm-Java Virtual Machine- 구동시작점
+    public static void main(String [] args){}
+    public : 아무데서나 접근 가능 / static : 메모리상 사용 가능 / void main(String) [] args) 전원 버튼 이름
+     
+ 5번째줄 : System.out.println("Hello");
+ 	System (운영체제) out(cmd) println(출력)
+ 	
+ 	Ctrl + F11 (실행)
+ */
