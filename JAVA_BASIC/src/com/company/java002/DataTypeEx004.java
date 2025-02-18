c348c4219532988940ce6fa8a27cee7f10269117
modoclou <cyj4336@gmail.com>
2025-02-18 18:21:34 +0900

    4 commit

--- JAVA_BASIC/src/com/company/java002/DataTypeEx004.java
+++ JAVA_BASIC/src/com/company/java002/DataTypeEx004.java
@@ -17,7 +17,7 @@ public class DataTypeEx004 {
 		
 		//처리 - 총점, 평균
 		total = kor + eng + math;
-		avg = total/3.0; // 정수/정수 = 정수  299/3  몫 > 해결방안) 정수/실수 or 실수/정수 
+		avg = total/3.0; // 정수/정수 = 정수  299/3  몫 > 해결방안) 정수/실수 or 실수/정수
 		
 		//출력 - 총점, 평균 출력
 		System.out.printf("총점 : %d \n평균: %.2f", total, avg);
